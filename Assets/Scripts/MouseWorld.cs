eec0051d24b56d0d484b3971c6335b8042f0165e
miguelaarrones <miguelaarrones@icloud.com>
2024-01-15 00:01:27 +0100

    Unit movement
    Using mouse world position to move the unit when left clicking.

---
+++ Assets/Scripts/MouseWorld.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseWorld : MonoBehaviour
+{
+    public static MouseWorld Instance { get; private set; }
+
+    [SerializeField] private LayerMask mousePlaneLayerMask;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public static Vector3 GetPosition()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, Instance.mousePlaneLayerMask);
+        return raycastHit.point;
+    }
+}
