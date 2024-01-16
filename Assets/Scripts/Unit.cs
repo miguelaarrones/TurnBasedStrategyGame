f07bb84e44e269c39f14c51ac5bf97c6f6778b8d
miguelaarrones <miguelaarrones@icloud.com>
2024-01-16 13:00:40 +0100

    Unit basic animations

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
+    private const string UNIT_ISWALKING = "IsWalking";
+
+    [SerializeField] private Animator unitAnimator;
+
     private Vector3 targetPosition;
 
     private void Update()
@@ -13,9 +17,17 @@ public class Unit : MonoBehaviour
         if (Vector3.Distance(transform.position, targetPosition) > stoppingDistance)
         {
             Vector3 moveDirection = (targetPosition - transform.position).normalized;
+            
             float moveSpeed = 4f;
-
             transform.position += moveDirection * moveSpeed * Time.deltaTime;
+
+            float rotationSpeed = 10f;
+            transform.forward = Vector3.Lerp(transform.forward, moveDirection, Time.deltaTime * rotationSpeed);
+
+            unitAnimator.SetBool(UNIT_ISWALKING, true);
+        } else
+        {
+            unitAnimator.SetBool(UNIT_ISWALKING, false);
         }
 
         if (Input.GetMouseButtonDown(0))
