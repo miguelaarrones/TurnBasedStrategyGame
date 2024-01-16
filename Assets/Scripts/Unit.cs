9044308db775e377eb0ca8807bfcdd212426656e
miguelaarrones <miguelaarrones@icloud.com>
2024-01-16 22:22:38 +0100

    UnitActionSystem - Unit selection

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -11,6 +11,11 @@ public class Unit : MonoBehaviour
 
     private Vector3 targetPosition;
 
+    private void Awake()
+    {
+        targetPosition = transform.position;
+    }
+
     private void Update()
     {
         float stoppingDistance = .1f;
@@ -29,15 +34,9 @@ public class Unit : MonoBehaviour
         {
             unitAnimator.SetBool(UNIT_ISWALKING, false);
         }
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            Move(MouseWorld.GetPosition());
-        }
-        
     }
 
-    private void Move(Vector3 targetPosition)
+    public void Move(Vector3 targetPosition)
     {
         this.targetPosition = targetPosition;
     }
---
+++ Assets/Scripts/UnitSelectedVisual.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitSelectedVisual : MonoBehaviour
+{
+    [SerializeField] private Unit unit;
+    
+    private MeshRenderer meshRenderer;
+
+    private void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        UpdateVisual();
+    }
+
+    private void Start()
+    {
+        UnitActionSystem.Instance.OnSelectedUnitChanged += UnitActionSystem_OnSelectedUnitChanged;
+    }
+
+    private void UnitActionSystem_OnSelectedUnitChanged(object sender, System.EventArgs e)
+    {
+        UpdateVisual();
+    }
+
+    private void UpdateVisual()
+    {
+        if (UnitActionSystem.Instance.GetSelectedUnit() == unit)
+        {
+            Show();
+        }
+        else
+        {
+            Hide();
+        }
+    }
+
+    private void Show()
+    {
+        meshRenderer.enabled = true;
+    }
+
+    private void Hide()
+    {
+        meshRenderer.enabled = false;
+    }
+}
