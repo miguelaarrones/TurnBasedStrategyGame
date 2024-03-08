a0859254ce1dc654ca86d0471ae6188d666f064d
miguelaarrones <miguelaarrones@icloud.com>
2024-03-08 13:15:33 +0100

    Destroyed crate visual
    Sliced the visuals for the crate and added explosion force when destroyed

--- Assets/Scripts/DestructibleCrate.cs
+++ Assets/Scripts/DestructibleCrate.cs
@@ -4,8 +4,26 @@ using UnityEngine;
 
 public class DestructibleCrate : MonoBehaviour
 {
+    [SerializeField] private Transform crateDestroyedPrefab;
+
     public void Damage()
     {
+        Transform crateDestroyedTransform = Instantiate(crateDestroyedPrefab, transform.position, transform.rotation);
+
+        ApplyExplosionToChildren(crateDestroyedTransform, 150f, transform.position, 10f);
+
         Destroy(gameObject);
+    }
+
+    private void ApplyExplosionToChildren(Transform root, float explosionForce, Vector3 explosionPosition, float explosionRange)
+    {
+        foreach (Transform child in root)
+        {
+            if (child.TryGetComponent<Rigidbody>(out Rigidbody childRigidbody))
+            {
+                childRigidbody.AddExplosionForce(explosionForce, explosionPosition, explosionRange);
+                ApplyExplosionToChildren(child, explosionForce, explosionPosition, explosionRange);
+            }
+        }
     }
 }
